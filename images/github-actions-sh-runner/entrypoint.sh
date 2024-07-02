8480e4cfa174b9f8a9dd9457f6bee95afdc66b43
Michael Akridge <michael.akridge@noaa.gov>
2024-07-02 11:43:16 -1000

    cleanup

--- images/github-actions-sh-runner/entrypoint.sh
+++ images/github-actions-sh-runner/entrypoint.sh
@@ -15,12 +15,15 @@ fi
 # Fetch the runner token using the GitHub API if GH_PAT is provided
 if [ -n "$GH_PAT" ]; then
   echo "Fetching the runner token for $GH_REPO_URL using GH_PAT..."
-  GH_RUNNER_TOKEN=$(curl -sX POST -H "Authorization: token ${GH_PAT}" ${GH_REPO_URL}/actions/runners/registration-token | jq -r .token)
+  response=$(curl -sX POST -H "Authorization: token ${GH_PAT}" ${GH_REPO_URL}/actions/runners/registration-token)
+  GH_RUNNER_TOKEN=$(echo $response | jq -r .token)
 
   if [ -z "$GH_RUNNER_TOKEN" ]; then
     echo "Failed to fetch runner token"
+    echo "Response from GitHub API: $response"
     exit 1
   fi
+
   echo "Runner token fetched successfully"
 fi
 
